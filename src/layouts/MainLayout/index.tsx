1476ddb420dfee9c05c90ac71de477ea54568bef
Michael Frieze <michaelfrieze@icloud.com>
2023-04-05 20:55:51 -0400

    implemented sign in auth using NextAuth

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { IoReorderThreeOutline } from "react-icons/io5";
 import { BsBell } from "react-icons/bs";
 import { FiEdit } from "react-icons/fi";
+import { signIn, useSession } from "next-auth/react";
 
 const MainLayout = ({ children }: React.PropsWithChildren) => {
+  const { data: sessionData, status } = useSession();
+  console.log(sessionData);
+
   return (
     <div className="flex h-full w-full flex-col">
       <header className="flex h-20 w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white">
@@ -11,22 +15,33 @@ const MainLayout = ({ children }: React.PropsWithChildren) => {
           <IoReorderThreeOutline className="text-2xl text-gray-600" />
         </div>
         <div className="text-xl font-thin">Ultimate Blog App</div>
-        <div className="flex items-center space-x-4">
-          <div>
-            <BsBell className="text-2xl text-gray-600" />
+        {status === "authenticated" ? (
+          <div className="flex items-center space-x-4">
+            <div>
+              <BsBell className="text-2xl text-gray-600" />
+            </div>
+            <div>
+              <div className="h-5 w-5 rounded-full bg-gray-600" />
+            </div>
+            <div>
+              <button className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900">
+                <div>Write</div>
+                <div>
+                  <FiEdit />
+                </div>
+              </button>
+            </div>
           </div>
+        ) : (
           <div>
-            <div className="h-5 w-5 rounded-full bg-gray-600" />
-          </div>
-          <div>
-            <button className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900">
-              <div>Write</div>
-              <div>
-                <FiEdit />
-              </div>
+            <button
+              onClick={() => signIn()}
+              className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
+            >
+              Sign In
             </button>
           </div>
-        </div>
+        )}
       </header>
 
       {children}
